ddd9d0b52cc4f8d0b59bda793ff5b328ee37f84a
Emmy Manning <emswoo@EM.local>
2025-09-03 10:36:43 -0400

    button function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,30 @@
-import Logo from '../assets/logo/logo.svg'
+import Button from './Button';
+import Logo from '../assets/logo/logo.svg';
+import Menu from '../assets/icons/menu.svg';
 
-const Header = () => {
-  return (
-    <header className='p-3'>
-        <div>
-            <img src={Logo} alt="Logo" width={48} height={48} />
-        </div>
-    </header>
-  )
+interface HeaderProps {
+  onClickHeroModal: () => void;
 }
 
-export default Header
+const Header: React.FC<HeaderProps> = ({ onClickHeroModal }) => {
+  return (
+    <header className='flex flex-col items-center justify-between w-28 h-screen'>
+      <div className='flex flex-col justify-between h-full'>
+        <div className='flex flex-col items-center'>
+          <img src={Logo} alt='Logo' width={50} height={50} className='pt-3 pb-1' />
+          <p className='text-main text-lg font-main'>OIKAZE</p>
+        </div>
+        <div className='flex justify-center pb-8'>
+          <Button onClick={onClickHeroModal} className='w-[28px] h-[28px]'>
+            <img src={Menu} alt='Menu' />
+          </Button>
+        </div>
+      </div>
+      <div className='flex justify-center items-center bg-main w-28 h-28'>
+        <p className='text-white font-main'>RESERVATION</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/HeroSection.tsx
+++ src/pages/HeroSection.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import Header from '../components/Header';
+import HeroImage from '../assets/hero_image.jpg';
+import HeroModal from '../components/modal/HeroModal';
 
 const HeroSection = () => {
+  const [openHeroModal, setOpenHeroModal] = useState(false);
+
+  const onClickHeroModal = () => {
+    setOpenHeroModal(true);
+  };
+
   return (
     <div>
-      <Header />
+      <div className='flex'>
+        <Header onClickHeroModal={onClickHeroModal} />
+        <img src={HeroImage} alt='Image' className='object-cover w-full h-screen' />
+      </div>
+      {openHeroModal && <HeroModal />}
     </div>
   );
 };
