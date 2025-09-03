7d4f9d9e2a117e2ce71b912b0c1be62763db3ed4
Emmy Manning <emswoo@EM.local>
2025-09-03 12:52:01 -0400

    modal function

--- src/components/modal/HeroModal.tsx
+++ src/components/modal/HeroModal.tsx
@@ -9,7 +9,7 @@ interface HeroModalProps {
 const HeroModal: React.FC<HeroModalProps> = ({ isOpen, onClose }) => {
   return (
     <div>
-      <div className={`fixed bottom-0 bg-white w-full h-[480px] translate-all duration-300 ease-out ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4 pointer-events-none'}`}>
+      <div className={`fixed bottom-0 bg-white w-full h-[50vh] translate-all duration-700 ease-out ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4 pointer-events-none'}`}>
         <Button onClick={onClose} className='cursor-pointer hover:opacity-70 mt-4 ml-4 w-[28px] h-[28px]'>
           <img src={Close} alt='Close' />
         </Button>
--- src/pages/HeroSection.tsx
+++ src/pages/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from '../components/Header';
 import HeroImage from '../assets/hero_image.jpg';
+import SubImage from '../assets/sub_image.jpg';
 import HeroModal from '../components/modal/HeroModal';
 
 const HeroSection = () => {
@@ -10,7 +11,10 @@ const HeroSection = () => {
     <div>
       <div className='flex'>
         <Header onOpen={() => setOpenHeroModal(true)} />
-        <img src={HeroImage} alt='Image' className='object-cover w-full h-screen' />
+        <div className='relative w-full'>
+          <img src={HeroImage} alt='HeroImage' className={`absolute inset-0 object-cover w-full h-screen transition-opacity duration-700 ${openHeroModal ? 'opacity-0' : 'opacity-100'}`} />
+          <img src={SubImage} alt='SubImage' className={`absolute inset-0 object-cover w-full h-[50vh] transition-opacity duration-700 ${openHeroModal ? 'opacity-100' : 'opacity-0'}`} />
+        </div>
       </div>
       <HeroModal isOpen={openHeroModal} onClose={() => setOpenHeroModal(false)} />
     </div>
